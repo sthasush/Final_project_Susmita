af35b962ffeb0f980d267667eba537ca7bf4c6c3
“sthasush git config --global user.name “sthasush <stha.sush94@gmail.com>
2021-04-16 15:37:03 -0500

    point graph to line graph

--- Jomsom_temp_graph.R
+++ Jomsom_temp_graph.R
@@ -10,13 +10,10 @@ library(ggpubr)
 jomsom <- data.frame(x= Jomsom$Date, y= c(Jomsom$tmax, Jomsom$tmin),
                     group = c(rep("tmax", nrow(Jomsom)),
                               rep("tmin", nrow(Jomsom))))
-View(Jomsom)
-my.formula= y~ poly(x,2)
-formula= y~x
 
 jomsom %>%
   ggplot(aes(x,y, color=group, na.rm=T))+
-  geom_point()+
+  geom_line()+
   theme_bw()+
   geom_smooth(method="lm", se=T)+
   scale_color_discrete(name='Temperature', labels=c('Maximum','Minimum'))+
@@ -24,10 +21,6 @@ jomsom %>%
   facet_wrap(~group, ncol=1,labeller = labeller(group = 
                                                   c("tmax" = "Maximum Temperature",
                                                     "tmin" = "Minimum Temperature")))
-  #stat_regline_equation(formula=formula,
-                        #aes(label=paste(..eq.label..,..rr.label..,sep='~~~')))
-  #stat_poly_eq(formula=my.formula,
-               #aes(label=paste(..eq.label..,..rr.label..,sep='~~~'), size=5))
 
 
-ggsave("Jomsom_temp_lm_seperate_pt.jpg", width=10, height=5)
+ggsave("Jomsom_temp_lm.jpg", width=9, height=5)
